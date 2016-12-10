b561b07c3c3eb5f3c08ac4ac58dbfeea55ab31cc
Rui Cerqueira <rvicerqveira@gmail.com>
2016-12-10 00:40:54 +0000

    completed challenge

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Request Header Parser Microservice
+## User stories:
+1) I can get the IP address, language and operating system for my browser.
+## Example usage:
+```
+https://rvicerqveira-header-parser-api.herokuapp.com/
+```
+## Example output:
+```
+{"ipaddress":"188.81.104.67","language":"pt-PT","software":"Windows NT 6.2"}
+```
--- server.js
+++ server.js
@@ -3,7 +3,13 @@ var app = express()
 var port = process.env.PORT || 8080
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  var info = {
+    'ipaddress': req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress,
+    'language': req.headers["accept-language"].split(',')[0],
+    'software': req.headers['user-agent'].split('(')[1].split(')')[0]
+  }
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(info))
 })
 
 app.listen(port, function () {
